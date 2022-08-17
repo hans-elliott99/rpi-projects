6065deade7611907a0933ff9b18c5c07f1263546
Hans Elliott <hanselliott61@gmail.com>
2022-08-16 17:33:29 -0700

    start repo, button.py

---
+++ button.py
@@ -0,0 +1,22 @@
+from gpiozero import Button
+from signal import pause
+
+GPIO = 4
+button = Button(GPIO, hold_time=2.5)
+
+def button_pressed():
+    print("Button was pressed.")
+
+def button_held():
+    print("Button was held.")
+
+def button_released():
+    print("Button released.")
+
+#button.when_pressed = button_pressed ##runs function when button is pressed
+#button.when_held = button_held  ##runs function when button is held for 2.5 sec
+#button.when_released = button_released ##runs function when button is released
+
+#pause()  ##keeps program listening for events
+print(2+2)
+
