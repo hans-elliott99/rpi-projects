2b95bee07ebcc2692283363ebd7c72acb08cd4ca
Hans Elliott <hanselliott61@gmail.com>
2022-08-16 17:58:19 -0700

    editing button from pi

--- button.py
+++ button.py
@@ -2,19 +2,26 @@ from gpiozero import Button
 from signal import pause
 
 GPIO = 4
+press_counter = 0
+press_limit = 3
 button = Button(GPIO, hold_time=2.5)
 
 def button_pressed():
-    print("Button was pressed.")
+    print("PRESSED.")
+    #press_counter += 1
 
 def button_held():
-    print("Button was held.")
+	print("HELD.")
 
 def button_released():
-    print("Button released.")
+	print("RELEASED.")
 
 button.when_pressed = button_pressed ##runs function when button is pressed
 button.when_held = button_held  ##runs function when button is held for 2.5 sec
 button.when_released = button_released ##runs function when button is released
+
+if press_counter >= press_limit:
+    print("that's a lot of pressing")
+    press_counter = 0
 
 pause()  ##keeps program listening for events
